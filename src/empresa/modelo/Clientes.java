6ffa7f823f0374d5653e288caf7e7111e0a75cbd
Formacion <Formacion@host.docker.internal>
2020-07-03 17:41:39 +0200

    cliente completado

---
+++ src/empresa/modelo/Clientes.java
@@ -0,0 +1,109 @@
+package empresa.modelo;
+
+import java.util.Date;
+
+public class Clientes extends Persona {
+	
+	private int puntosAcumulados;
+	private float saldo;
+	private String usuario;
+	private String password;
+	
+	public Clientes() {
+	}
+
+	public Clientes(String nombre, String apellido1, String apellido2, String dni, String email, int edad, char sexo,
+			Date fechaAlta, Date fechaBaja, int puntosAcumulados, float saldo, String usuario, String password) {
+		super(nombre, apellido1, apellido2, dni, email, edad, sexo, fechaAlta, fechaBaja);
+		this.puntosAcumulados = puntosAcumulados;
+		this.saldo = saldo;
+		this.usuario = usuario;
+		this.password = password;
+	}
+
+	public Clientes(int puntosAcumulados, float saldo, String usuario, String password) {
+		this.puntosAcumulados = puntosAcumulados;
+		this.saldo = saldo;
+		this.usuario = usuario;
+		this.password = password;
+	}
+
+	public int getPuntosAcumulados() {
+		return puntosAcumulados;
+	}
+
+	public void setPuntosAcumulados(int puntosAcumulados) {
+		this.puntosAcumulados = puntosAcumulados;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + puntosAcumulados;
+		result = prime * result + Float.floatToIntBits(saldo);
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Clientes other = (Clientes) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (puntosAcumulados != other.puntosAcumulados)
+			return false;
+		if (Float.floatToIntBits(saldo) != Float.floatToIntBits(other.saldo))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Clientes [puntosAcumulados=" + puntosAcumulados + ", saldo=" + saldo + ", usuario=" + usuario
+				+ ", password=" + password + "]";
+	}
+	
+	
+	
+
+}
